729e998ac4b17f94c0a7e6b018fd9448cfc25b5d
Somondas <somondas067@gmail.com>
2026-02-10 16:08:57 +0530

    Updates

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,15 +4,16 @@ import * as dotenv from "dotenv";
 // Load environment variables from .env
 dotenv.config({ path: ".env" });
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error("POSTGRES_URL is not set in .env");
-}
+// if (!process.env.POSTGRES_URL) {
+//   throw new Error("POSTGRES_URL is not set in .env");
+// }
 
+console.log("Using POSTGRES_URL:", process.env.POSTGRES_URL);
 export default defineConfig({
   schema: "./lib/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL,
+    url: process.env.POSTGRES_URL || "",
   },
 });
